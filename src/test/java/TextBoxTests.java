e141cc5356fb62477389307d2b65c7ace1d65d06
egosteva <elenagosteva.qa@gmail.com>
2023-03-02 20:11:21 +0300

    added TextBoxTests

---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest(){
+        open("/text-box");
+
+        $("#userName").setValue("Andrey Fetisov");
+        $("#userEmail").setValue("fetisov@somemail.ru");
+        $("#currentAddress").setValue("Moscow, Arbat St");
+        $("#permanentAddress").setValue("NY");
+        $("#submit").click();
+
+        $("#submit").click();
+        $("#output").shouldHave(text("Andrey Fetisov"), text("fetisov@somemail.ru"),
+                text("Moscow, Arbat St"), text("NY"));
+    }
+}
