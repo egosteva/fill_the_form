5cc1b3e6a1c6214654353b8eaa8bea2e2611af31
egosteva <elenagosteva.qa@gmail.com>
2023-03-15 10:46:50 +0300

    cadded TestBase

--- src/test/java/tests/PracticeFormWithPageObjects.java
+++ src/test/java/tests/PracticeFormWithPageObjects.java
@@ -1,24 +1,13 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import pages.RegistrationPage;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 
-public class PracticeFormWithPageObjects {
-    RegistrationPage registrationPage = new RegistrationPage();
-
-
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-    }
+public class PracticeFormWithPageObjects extends TestBase {
 
     @Test
     void fillFormTest() {
